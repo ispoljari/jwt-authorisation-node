287e5b621ca0d6f08b49a5b7cf9651d8f4bb116a
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-10-13 17:33:01 +0200

    Implement the runServer function

--- server.js
+++ server.js
@@ -7,3 +7,18 @@ const morgan = require('morgan');
 const passport = require('passport');
 
 mongoose.Promise = global.Promise;
+
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+  mongoose.connect(databaseUrl, err => {
+    if (err) {
+      console.error(err);
+      return 
+    }
+
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+    });
+  });
+}
