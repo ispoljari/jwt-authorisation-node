fedea76a58df1f7efb0deb51e1275ba5b7c0c083
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-10-13 18:08:41 +0200

    Setup CORS middleware

--- server.js
+++ server.js
@@ -20,6 +20,18 @@ const app = express();
 // Logging
 app.use(morgan('common'));
 
+// CORS
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  if(req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+
+  next();
+});
+
 // Run / Close Server
 
 let server;
