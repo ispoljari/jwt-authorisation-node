0c494855ca6faf4c9a3107de0d4b3d2b84f9a1db
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-10-16 10:01:11 +0200

    Implement POST /users endpoint with username/password existence validation

--- users/users.router.js
+++ users/users.router.js
@@ -4,3 +4,19 @@ const express = require('express');
 const {User} = require('./models');
 
 const router = express.Router();
+
+router.post('/', (req, res) => {
+
+  // Check if username and password are provided in the request body
+  const requiredFields = ['username', 'password'];
+  const missingFields = requiredFields.find(field => !(field in req.body));
+
+  if (missingFields) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Missing field',
+      location: missingFields
+    });
+  }
+});
