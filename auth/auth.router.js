8c633e1b64f63f4bd55b29ca1be504a7d2933be5
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-10-16 16:09:41 +0200

    Install packages jsonwebtoken and passport-local

--- auth/auth.router.js
+++ auth/auth.router.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 const localAuth = passport.authenticate('local', {session: false});
