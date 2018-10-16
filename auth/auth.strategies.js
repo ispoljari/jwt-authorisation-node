4e941b6294c36db3ea6f682de31455dfe514089c
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-10-16 16:02:18 +0200

    Implement local authorization with passportJS

---
+++ auth/auth.strategies.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const {Strategy: LocalStrategy} = require('passport-local');
+
+const {User} = require('../users/users.model');
+
+const localStrategy = new LocalStrategy((username, password, callback) => {
+  let user; 
+
+  User.findOne({username})
+    .then(_user => {
+      user = _user;
+
+      if (!user) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect username or password'
+        });
+      }
+      return user.validatePassword(password);
+    })
+    .then(isvalid => {
+      if (!isvalid) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect username or password'
+        });
+      }
+
+      return callback(null, user);
+    })
+    .catch(err => {
+      if (err.reason === 'LoginError') {
+        return callback(null, false, err);
+      }
+
+      return callback(err, false);
+    })
+});
